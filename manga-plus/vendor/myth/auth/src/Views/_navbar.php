46b2ed194f00e25998a336692483c8b82d7b915d
miaowmerE <54744527+trueazp@users.noreply.github.com>
2020-12-17 13:05:16 +0800

    remove vendor/ from .gitignore

---
+++ manga-plus/vendor/myth/auth/src/Views/_navbar.php
@@ -0,0 +1,33 @@
+<div class="ui inverted large top fixed main menu">
+
+    <div class="item">
+        <img src="/mangaplus.ico">
+    </div>
+
+    <a href="<?= base_url('/'); ?>" class="item">
+        <div>
+            <i class="home icon"></i>Home
+        </div>
+    </a>
+
+    <a href="<?= base_url('/user/user_komik'); ?>" class="item">
+        <div>
+            <i class="book icon"></i>Comics
+        </div>
+    </a>
+
+    <a href="<?= base_url('https://github.com/trueazp'); ?>" class="item" target="__blank">
+        <div>
+            <i class="user icon"></i>About Me
+        </div>
+    </a>
+
+
+    <a href="<?= base_url('/login'); ?>" class="item right">
+        <div>
+            <i class="sign in icon"></i>Login
+        </div>
+    </a>
+
+
+</div>
